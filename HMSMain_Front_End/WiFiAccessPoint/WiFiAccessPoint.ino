023396a4f7e6130fdce497b472a5705165cdbe95
Mike Molinari <mike@smbisoft.com>
2021-11-27 20:29:01 -0500

    Added wifi connect with failover.

--- HMSMain_Front_End/WiFiAccessPoint/WiFiAccessPoint.ino
+++ HMSMain_Front_End/WiFiAccessPoint/WiFiAccessPoint.ino
@@ -8,8 +8,8 @@
 #define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
 
 // Set these to your desired credentials.
-const char *ssid = "yourAP";
-const char *password = "yourPassword";
+const char *ssid = "H-BAT-" + (String)WiFi.macAddress();
+const char *password = "hbat";
 
 WebServer server(80);
 
@@ -19,11 +19,9 @@ void setup() {
   Serial.println();
   Serial.println("Configuring access point...");
 
-  // You can remove the password parameter if you want the AP to be open.
-  WiFi.softAP(ssid, password);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  connectToApWithFailToStation();
+
+
 
 
   Serial.println("Server started");
@@ -32,6 +30,15 @@ void setup() {
 
   server.on(F("/"), []() {
     server.send(200, "text/html", indexHtml);
+  });
+
+
+  
+  server.on(F("/wifiUpdate"), []() {
+    //Place code here to setup wifi connectivity
+    String NewApName = server.arg("apName");
+    String NewApPass = server.arg("apPass");
+    server.send(200, "application/json", jsonString);
   });
 
   
@@ -44,17 +51,6 @@ void setup() {
     server.send(200, "application/json", jsonText);
   });
 
-//  server.on(UriBraces("/users/{}"), []() {
-//    String user = server.pathArg(0);
-//    server.send(200, "text/plain", "User: '" + user + "'");
-//  });
-//
-//  server.on(UriRegex("^\\/users\\/([0-9]+)\\/devices\\/([0-9]+)$"), []() {
-//    String user = server.pathArg(0);
-//    String device = server.pathArg(1);
-//    server.send(200, "text/plain", "User: '" + user + "' and Device: '" + device + "'");
-//  });
-
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -66,4 +62,30 @@ String json_return_data(){
 void loop() {
   server.handleClient();
   delay(0);
+}
+
+void connectToApWithFailToStation(String WIFI_STA_SSID, String WIFI_STA_PASS)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_STA_SSID, WIFI_STA_PASS);
+
+  int numberOfAttempts = 0;
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print(".");
+    if (numberOfAttempts > 50)
+    {
+      WiFi.mode(WIFI_AP);
+      // You can remove the password parameter if you want the AP to be open.
+      WiFi.softAP(ssid, password);
+      Serial.print("Wifi Connect Failed. \r\nStarting AP. \r\nAP IP address: ");
+      Serial.println(WiFi.softAPIP());
+      break;
+    }
+  }
+  Serial.println();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 }

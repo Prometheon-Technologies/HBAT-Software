8c27186898ad12baf6e9f39a92cd6f6b3a0e28d5
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-04-08 00:10:36 +0200

    update

--- HMSmain_Back_End/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zacar/Documents/GitHub/HBAT-Software/HMSmain_Back_End
-# Build directory: C:/Users/zacar/Documents/GitHub/HBAT-Software/HMSmain_Back_End/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

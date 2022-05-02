a6bc27b8000b7db7e8b349f316c387dd68a52649
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-05-02 15:12:58 +0200

    update

--- HBAT/lib/HMSmain/src/HMSNetwork.cpp
+++ HBAT/lib/HMSmain/src/HMSNetwork.cpp
@@ -1,3 +1,33 @@
+/**
+ * WiFi Events
+ *0  SYSTEM_EVENT_WIFI_READY               < ESP32 WiFi ready
+ *1  SYSTEM_EVENT_SCAN_DONE                < ESP32 finish scanning AP
+ *2  SYSTEM_EVENT_STA_START                < ESP32 station start
+ *3  SYSTEM_EVENT_STA_STOP                 < ESP32 station stop
+ *4  SYSTEM_EVENT_STA_CONNECTED            < ESP32 station connected to AP
+ *5  SYSTEM_EVENT_STA_DISCONNECTED         < ESP32 station disconnected from AP
+ *6  SYSTEM_EVENT_STA_AUTHMODE_CHANGE      < the auth mode of AP connected by ESP32 station changed
+ *7  SYSTEM_EVENT_STA_GOT_IP               < ESP32 station got IP from connected AP
+ *8  SYSTEM_EVENT_STA_LOST_IP              < ESP32 station lost IP and the IP is reset to 0
+ *9  SYSTEM_EVENT_STA_WPS_ER_SUCCESS       < ESP32 station wps succeeds in enrollee mode
+ *10 SYSTEM_EVENT_STA_WPS_ER_FAILED        < ESP32 station wps fails in enrollee mode
+ *11 SYSTEM_EVENT_STA_WPS_ER_TIMEOUT       < ESP32 station wps timeout in enrollee mode
+ *12 SYSTEM_EVENT_STA_WPS_ER_PIN           < ESP32 station wps pin code in enrollee mode
+ *13 SYSTEM_EVENT_AP_START                 < ESP32 soft-AP start
+ *14 SYSTEM_EVENT_AP_STOP                  < ESP32 soft-AP stop
+ *15 SYSTEM_EVENT_AP_STACONNECTED          < a station connected to ESP32 soft-AP
+ *16 SYSTEM_EVENT_AP_STADISCONNECTED       < a station disconnected from ESP32 soft-AP
+ *17 SYSTEM_EVENT_AP_STAIPASSIGNED         < ESP32 soft-AP assign an IP to a connected station
+ *18 SYSTEM_EVENT_AP_PROBEREQRECVED        < Receive probe request packet in soft-AP interface
+ *19 SYSTEM_EVENT_GOT_IP6                  < ESP32 station or ap or ethernet interface v6IP addr is preferred
+ *20 SYSTEM_EVENT_ETH_START                < ESP32 ethernet start
+ *21 SYSTEM_EVENT_ETH_STOP                 < ESP32 ethernet stop
+ *22 SYSTEM_EVENT_ETH_CONNECTED            < ESP32 ethernet phy link up
+ *23 SYSTEM_EVENT_ETH_DISCONNECTED         < ESP32 ethernet phy link down
+ *24 SYSTEM_EVENT_ETH_GOT_IP               < ESP32 ethernet got IP from connected AP
+ *25 SYSTEM_EVENT_MAX
+ */
+
 #include "HMSnetwork.hpp"
 
 AsyncWebServer server(80);
@@ -23,6 +53,10 @@ String ledState;
 unsigned long previousMillis = 0;
 const long interval = 30000; // interval to wait for Wi-Fi connection (milliseconds)
 
+/**
+ * @brief Construct a new HMSnetwork::HMSnetwork object
+ * 
+ */
 HMSnetwork::HMSnetwork()
 {
     // constructor
@@ -73,24 +107,9 @@ void notFound(AsyncWebServerRequest *request)
     request->send(404, "text/plain", "Not found.");
 }
 
-std::vector<String> splitStringToVector(String msg)
-{
-    std::vector<String> subStrings;
-    int j = 0;
-    for (int i = 0; i < msg.length(); i++)
-    {
-        if (msg.charAt(i) == '.')
-        {
-            subStrings.push_back(msg.substring(j, i));
-            j = i + 1;
-        }
-    }
-    subStrings.push_back(msg.substring(j, msg.length())); // to grab the last value of the string
-    for (int x = 0; x < subStrings.size(); x++)
-    {
-        log_i("[INFO]: %s\n", subStrings[x].c_str());
-    }
-    return subStrings;
+void checkClientConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Connected to AP successfully!");
+  digitalWrite(LED_PIN_1, HIGH);
 }
 
 bool HMSnetwork::SetupNetworkStack()
